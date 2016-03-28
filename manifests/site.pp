1593202b73ba62a98459847cde0944ad13810627
jexley <Jack.Exley@yahoo.com>
2016-03-28 15:29:44 -0400

    Managing /etc/motd
    Additions to /etc/motd

--- manifests/site.pp
+++ manifests/site.pp
@@ -49,6 +49,6 @@ node default {
     owner => 'root',
     group => 'root',
     mode => '0644',
-    content => 'Puppet code is fun to write',
+    content => 'Puppet code is fun to write, exley',
     }
 }
