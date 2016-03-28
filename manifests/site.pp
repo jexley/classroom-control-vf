cdbe015c1c5fba594c0c11995744f8e0902bdef5
jexley <Jack.Exley@yahoo.com>
2016-03-28 15:58:20 -0400

    execstatement
    Added exec statement

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,7 +43,7 @@ node default {
   # Example:
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
-  notify { "I just want this day to end": }
+  #notify { "I just want this day to end": }
   #file { '/etc/motd':
    # ensure => file,
     #owner => 'root',
